667ba3a5bbf426abdb3f680ab04f3b1c6e19f837
nsrchemie <nsrchemie@gmail.com>
2017-06-06 22:05:04 -0400

    arithmetic operations on input variables

---
+++ multInp.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+
+ cout << "Enter First Number: ";
+ int fNum;
+ cin >> fNum;
+
+ cout << "Enter Second Number: ";
+ int sNum;
+ cin >> sNum;
+
+ int prod = fNum * sNum;
+
+ cout << fNum << " x " << sNum;
+ cout << " = " << prod << endl;
+
+ return 0;
+}
