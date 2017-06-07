19f3c24f87ae4c7a4d4561d23a1a8017103762ae
nsrchemie <nsrchemie@gmail.com>
2017-06-07 00:04:20 -0400

    program that displays memory size (in bytes) of c++ types

---
+++ typeSize.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main()
+{
+using namespace std;
+ cout << "Size of bool:" << sizeof(bool) << endl;
+ cout << "Size of char:" << sizeof(char) << endl;
+ cout << "Size of unsigned short int" << sizeof(unsigned short) << endl;
+ cout << "Size of short int" << sizeof(short int) << endl;
+ cout << "Size of long long" << sizeof(long long) << endl;
+ cout << "Size of short" << sizeof(short) << endl;
+
+ cout << "Keep in mind that sizes vary with compiler, hardware and OS" << endl;
+
+ return 0;
+}
