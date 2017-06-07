2000f179d6ca009585dc422e9437f1898a8003dc
nsrchemie <nsrchemie@gmail.com>
2017-06-06 21:25:49 -0400

    working with basic fn declaratino and calling in main fn of program with differing c output

---
+++ nestFun.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int DemoConsoleOutput();
+
+int main ()
+{
+DemoConsoleOutput();
+return 0;
+}
+
+int DemoConsoleOutput()
+{
+ cout << "Simple string literal" <<endl;
+ cout << "Writing number five" << 5 << endl;
+ cout << "Calculating approximated Pi (22.0/7) = " << 22.0/7 << endl;
+
+ return 0;
+}
