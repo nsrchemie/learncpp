aee3aa4273cee98da553807f42bd5b7c66315e13
nsrchemie <nsrchemie@gmail.com>
2017-06-07 00:19:38 -0400

    playing around with auto keyword for type inference, Need to change compile format, e.g. g++ -o ProgramExe ProgramSource -std=c++11

---
+++ Usingauto.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ auto coinFlippedHeads = true;
+ auto largeNumber = 250000000000;
+
+ cout << "coinFlippedHeads value: " << coinFlippedHeads << endl;
+ cout << "size of coinFlippedHeads" << endl;
+
+ cout << "largeNumber value: " << largeNumber << endl;
+ cout << "largeNumber size: " << sizeof(largeNumber) << endl;
+ return 0;
+}
