9eb2cf6dea4a6cbb5fa5896271205691420a72db
nsrchemie <nsrchemie@gmail.com>
2017-06-08 10:58:55 -0400

    basic input and conditional reply

---
+++ eqcond.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ int x;
+ int y = 5;
+
+ cout << "Enter a number: " << endl;
+ cin >> x;
+
+ if (x == y) {
+ cout << "x and y are equal!" << endl;
+}
+
+return 0;
+}
