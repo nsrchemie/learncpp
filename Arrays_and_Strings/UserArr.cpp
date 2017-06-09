6cef1ea8030f974bde28c95900ee5e31c88b8bf7
nsrchemie <nsrchemie@gmail.com>
2017-06-09 15:43:19 -0400

    added some extra feedback, original val that is changed and return of length of array

--- Arrays_and_Strings/UserArr.cpp
+++ Arrays_and_Strings/UserArr.cpp
@@ -11,19 +11,20 @@ int main()
  //Create array with 25 slots
  int moreNums [Square(ARR_LEN)];
 
- cout << "Enter index of element to be changed: ";
+ cout << "Length of the array is " << distance(begin(myNums),end(myNums));
+ cout << "\nEnter index of element to be changed: \n";
  int elIndex;
  cin >> elIndex;
 
+ cout << "The value of that index in the array is: " << myNums[elIndex] << endl;
  cout << "Enter new value: ";
  int newVal;
  cin >> newVal;
 
  myNums[elIndex] = newVal;
 
- cout << "The element you selected in the array was " <<
- elIndex << " and the new value of myNums[" << elIndex << "] is "
- << myNums[elIndex] << endl;
+ cout << "The element you selected in the array was " << elIndex << endl;
+ cout << "The new value of array[" << elIndex << "] is "<< myNums[elIndex] << endl;
 
  return 0;
 }
