ac8d5b995a8056216796c386bc074d5a2fc2a7f7
nsrchemie <nsrchemie@gmail.com>
2017-06-14 23:48:57 -0400

    rewrite of goto and while loop as do...while loop

---
+++ Flow/dwh.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+ char userSelection = 'x';
+ do
+ {
+   cout << "Enter the two integers: " << endl;
+   int num1 = 0, num2 = 0;
+   cin >> num1;
+   cin >> num2;
+
+   cout << num1 << " x " << num2 << " = " << num1*num2 << endl;
+   cout << num1 << " + " << num2 << " = " << num1+num2 << endl;
+  } while (userSelection != 'x');
+
+  cout << "Goodbye!" << endl;
+
+ return 0;
+}
